3187b90759ee4bb95aea9869a578435da53b8173
IdoS88 <ishamir99@gmail.com>
2022-08-29 21:54:08 +0300

    class of resource

---
+++ dest/resource.js
@@ -0,0 +1,23 @@
+"use strict";
+class Resource {
+    constructor(name, amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+    set updateResourceAmount(amount) {
+        this.amount += amount;
+    }
+    get getResourceName() {
+        return this.name;
+    }
+    get getResourceAmount() {
+        return this.amount;
+    }
+}
+const item = new Resource("water", 100);
+item.updateResourceAmount = 4;
+let p = document.getElementById("status");
+if (p) {
+    p.innerHTML = item.getResourceAmount + item.getResourceName;
+}
+console.log(item.getResourceAmount + item.getResourceName);
